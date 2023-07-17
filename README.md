624d9466cc4f301149afa0021d7a99fdec6506ff
mariiapankova <pankova.maa@gmail.com>
2023-07-17 18:34:32 +0300

    Refactored and added README

---
+++ README.md
@@ -0,0 +1,21 @@
+# House Pricing
+
+A housing prices regression pet project. 
+
+Tech stack:
+    - pandas
+    - sklearn
+    - xgboost
+    - DVC for experiment tracking
+
+## Dataset
+
+[Housing Prices Dataset](https://www.kaggle.com/datasets/yasserh/housing-prices-dataset) has 13 features and 545 samples.
+
+All categorical columns were preprocessed usind one-hot encoding.
+
+## Results
+
+![](dvclive/plots/mse.png)
+
+![](dvclive/plots/r2.png)
--- src/dataprep.py
+++ src/dataprep.py
@@ -1,18 +1,20 @@
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler, OrdinalEncoder
 from sklearn.model_selection import (
     cross_validate,
-    cross_val_score,
     RepeatedKFold,
 )
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
 from dvclive import Live
 from dvc.api import params_show
 from rich import print
-from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBRegressor
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 RANDOM_STATE = 42
+sns.set_style()
 
 
 def df_prepare(csv_path: str):
@@ -41,24 +43,70 @@ def df_prepare(csv_path: str):
     return X, Y
 
 
+def get_model(model_params: dict):
+    yn_cols = [
+        "mainroad",
+        "guestroom",
+        "basement",
+        "hotwaterheating",
+        "airconditioning",
+        "prefarea",
+    ]
+    pipeline = Pipeline(
+        [
+            (
+                "column_transformer",
+                ColumnTransformer(
+                    [
+                        *(
+                            (f"{col}-binalizer", OrdinalEncoder(), [col])
+                            for col in yn_cols
+                        ),
+                        ("One-hot-furniture", OneHotEncoder(), ["furnishingstatus"]),
+                    ],
+                    remainder="passthrough",
+                ),
+            ),
+            (
+                "estimator",
+                XGBRegressor(**model_params, random_state=RANDOM_STATE),
+            ),
+        ]
+    )
+    return pipeline
+
+
 def main(csv_path: str = "data/Housing.csv"):
-    model_params = params_show()
-    model = XGBRegressor(**model_params)
-    X, Y = df_prepare(csv_path)
+    params = params_show()
+    model = get_model(params["model"])
+    print(model)
+    housing_data = pd.read_csv(csv_path)
+    X = housing_data.drop(["price"], axis="columns")
+    Y = MinMaxScaler().fit_transform(housing_data[["price"]])
+
     cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=RANDOM_STATE)
     scores = cross_validate(
         model,
         X,
         Y,
-        scoring={"acc": "accuracy", "presision": "precision", "rec": "recall"},
+        scoring={"neg_mse": "neg_root_mean_squared_error", "r2": "r2"},
         cv=cv,
         n_jobs=-1,
     )
+
+    sns.boxplot(scores["test_neg_mse"] * (-1)).set(xlabel="MSE")
+    plt.savefig("dvclive/plots/mse.png")
+    plt.clf()
+    sns.boxplot(scores["test_r2"]).set(xlabel="$r^2$")
+    plt.savefig("dvclive/plots/r2.png")
+
+    print(scores)
+
     with Live(save_dvc_exp=True) as live:
         for k, v in scores.items():
             live.log_metric(f"{k}_mean", v.mean())
-            live.log_metric(f"{k}_std", v.std())
+            print(f"{k}_mean", v.mean())
 
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     main()
